b504a0bc2cb90abf074d1d39a44eeeee842adb9b
Bolgash <47551806+Bolgash@users.noreply.github.com>
2019-10-03 10:43:33 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,24 +4,39 @@ using namespace std;
 
 int main()
 {
-    int a[100][100],n,i,j,m;
+    int a[100][100],b[100][100],n,i,j,osszeg,c[100][100];
+    cout<<"N=";
     cin>>n;
-cin>>m;
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-                cout<<"a["<<i<<"]["<<j<<"]"<<endl;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+
             cin>>a[i][j];
-
-
         }
     }
 
-for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-         cout<<a[i][j]<<" ";
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            cin>>b[i][j];
         }
-cout<<endl;
-}
+    }
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            c[i][j]=0;
+            for(int l=0; l<n; l++)
+            {
+                c[i][j]+=a[i][l]*b[l][j];
+            }
+            cout<<c[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
 
 
 
