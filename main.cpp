ea2697d407d96e3df544bcd96b3dab50ec0d1f0c
Bolgash <47551806+Bolgash@users.noreply.github.com>
2020-01-14 06:31:38 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,79 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    int a[100][100],b[100][100],n,i,j,osszeg,c[100][100];
-    cout<<"N=";
-    cin>>n;
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<n; j++)
-        {
+    ifstream be1("nr1.txt");
+    ifstream be2("nr2.txt");
 
-            cin>>a[i][j];
-        }
-    }
+    int n,m,v[100],p=0,k=0,aux;
 
-    for(i=0; i<n; i++)
+    be1>>n;
+    be2>>m;
+
+    for(int i=0; i<n; i++)
     {
-        for(j=0; j<n; j++)
-        {
-            cin>>b[i][j];
-        }
+        be1>>v[i];
     }
-    for(i=0; i<n; i++)
+       do
     {
-        for(j=0; j<n; j++)
+
+        p=0;
+
+        for (int i=0; i<n-1-k; i++)
+
         {
-            c[i][j]=0;
-            for(int l=0; l<n; l++)
+            if (v[i]>v[i+1])
             {
-                c[i][j]+=a[i][l]*b[l][j];
+                aux=v[i+1];
+                v[i+1]=v[i];
+                v[i]=aux;
+
+                p=1;
             }
-            cout<<c[i][j]<<" ";
-
         }
-        cout<<endl;
-    }
 
+        k++;
+    }
+    while(p==1);
+
+    for (int i=0; i<n; i++)
+    {
+
+        cout << v[i] << " ";
+    }
+    for(int j=0; j<m; j++)
+    {
+        be2>>v[j];
+    }
+    do
+    {
+
+        p=0;
+
+        for (int j=0; j<n-1-k; j++)
+
+        {
+            if (v[j]>v[j+1])
+            {
+                aux=v[j+1];
+                v[j+1]=v[j];
+                v[j]=aux;
+
+                p=1;
+            }
+        }
+
+        k++;
+    }
+    while(p==1);
+
+    for (j=0; j<n; j++)
+    {
+
+        cout << v[j] << " ";
+    }
 
 
 
